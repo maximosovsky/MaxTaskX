231beeef2702bcbef8097566320ed1fea1be8ebb
Maxim Osovsky <Maxim.Osovsky@gmail.com>
2025-05-28 13:59:13 +0300

    Set up Task Reminder PWA

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
